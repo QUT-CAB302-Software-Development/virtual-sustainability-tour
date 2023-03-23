a0cac6654845ad5f248a1206074b4fb6b5760a44
Nathan Wang <nathan123wang@gmail.com>
2023-03-23 13:36:12 +1000

    Initial test

--- demo/src/main/java/example/application/DemoApplication.java
+++ demo/src/main/java/example/application/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// Main entry point
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
