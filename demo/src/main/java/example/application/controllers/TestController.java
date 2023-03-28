03ab2b6202ae5eb494018a73583497950ab1ee5d
SuperRaptor22 <schen8523@gmail.com>
2023-03-28 12:33:30 +1000

    "able to serve react build now, serves on localhost:8080"

--- demo/src/main/java/example/application/controllers/TestController.java
+++ demo/src/main/java/example/application/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String index() {
         return "Greetings from Spring Boot!";
     }
