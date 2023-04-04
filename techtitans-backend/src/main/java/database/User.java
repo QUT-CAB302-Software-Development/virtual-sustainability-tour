a055e0a4eee5166f0c36901d39d715fb9fc1240e
Nathan Wang <nathan123wang@gmail.com>
2023-04-04 12:31:24 +1000

    Create user class

---
+++ techtitans-backend/src/main/java/database/User.java
@@ -0,0 +1,15 @@
+package database;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+}
