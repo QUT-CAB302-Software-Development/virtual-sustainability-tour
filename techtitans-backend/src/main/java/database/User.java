1bdbc0cf5310df8595a67add11315e49293f73e2
Nathan Wang <nathan123wang@gmail.com>
2023-04-07 13:07:55 +1000

    Add userid, username, and phone number fields to user class. Add method to find if user exists in list of class.

--- techtitans-backend/src/main/java/database/User.java
+++ techtitans-backend/src/main/java/database/User.java
@@ -2,16 +2,21 @@ package database;
 
 // Contains private user data for encapsulation
 public class User {
+    private String userid;
+    private String username;
+    private String phone;
     private String name;
     private String email;
     private String password;
 
     // Constructor for new user
-    public User(String name, String email, String password)
+    public User(String userid, String username, String phone, String name, String email, String password)
     {
+        this.userid = userid;
+        this.username = username;
+        this.phone = phone;
         this.name = name;
         this.email = email;
         this.password = password;
     }
-
 }
--- techtitans-backend/src/main/java/database/UserDatabase.java
+++ techtitans-backend/src/main/java/database/UserDatabase.java
@@ -7,18 +7,18 @@ public class UserDatabase {
     List<User> users = new ArrayList<>();
 
     // initialises new user, creates user, and adds to class list/
-    public User CreateUser(String name, String email, String password) {
-        User user = new User(name, email, password);
+    public User CreateUser(String userid, String username, String phone, String name, String email, String password) {
+        User user = new User(userid, username, phone, name, email, password);
         users.add(user);
         return user;
     }
 
-    // Check if email exists and returns email in list of class.
-    public User EmailExists(String email)
+    // Check if user exists and returns user in list of class.
+    public User UserExists(String userid)
     {
         for (User user : users)
         {
-            if (users.contains(email))
+            if (users.contains(userid))
             {
                 return user;
             }
