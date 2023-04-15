6ee8c3b8bdd87634ad0556438ce7c5349953ee0f
Rj Otadoy <otadoyrojimjude@gmail.com>
2023-04-15 17:11:59 +1000

    Fixed build issue

--- tecttitans-app/src/App.js
+++ tecttitans-app/src/App.js
@@ -7,13 +7,14 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Tour from './pages/Tour';
 import Contact from './pages/Contact';
+
+//  established routing between the four pages of the front end
 function App() {
   return (
       <>
           <Router>
               <Navbar />
               <Routes>
-              // established routing between the four pages of the front end
                   <Route path='/' exact element={Home()} />
                   <Route path='/sign-up' element={SignUp()} />
                   <Route path='/tour' element={Tour()} />
