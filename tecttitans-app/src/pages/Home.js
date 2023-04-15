2ef8419864c5093ffcc503d4075703f5766237bc
Rj Otadoy <otadoyrojimjude@gmail.com>
2023-04-15 10:31:24 +1000

    Initial commit

--- tecttitans-app/src/pages/Home.js
+++ tecttitans-app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
 import HeroSection from '../components/HeroSection';
-
+import Cards from '../components/cards';
 function Home() {
     return (
         <>
             <HeroSection />
+            <Cards />
         </>
     );
 }
