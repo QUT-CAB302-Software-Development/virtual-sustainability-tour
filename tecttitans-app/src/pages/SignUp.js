98271a321130ef397508485887e10b585368987a
Rj Otadoy <otadoyrojimjude@gmail.com>
2023-04-16 16:59:40 +1000

    Issue Ticket: #30
    * Front-end of register is complete
    * needs to add error handling features
    * Fixed build issue
    
    [/] I have performed a self-review of my code
    [/] I have tested the extensively tested the code
    [/] Added comments to code to follow best practice

--- tecttitans-app/src/pages/SignUp.js
+++ tecttitans-app/src/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import FormInput from '../components/FormInput';
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 function SignUp(){
 
