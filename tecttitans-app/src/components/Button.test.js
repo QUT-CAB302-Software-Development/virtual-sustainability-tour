6aacfefe4a153d048fffcf3c0a7e7eaa62eb7d90
Nelvin198 <75972787+Nelvin198@users.noreply.github.com>
2023-04-18 12:14:57 +1000

    Fixed minor errors such as using screen.getByRole instead of getByRole, direct node access etc

--- tecttitans-app/src/components/Button.test.js
+++ tecttitans-app/src/components/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom'
 import { Button } from './Button';
 import  '@testing-library/jest-dom/extend-expect';
@@ -7,9 +7,15 @@ import  '@testing-library/jest-dom/extend-expect';
 
 describe('Button', () => {
 
+    /*This tests whether a button component renders childeren and handles click events.
+    * It checks that when a user clicks on the button, if the button component is working and passing as a prop(properties)
+    * For example, when the button text = "Click me!", the value of the text prop is "Click me!"*/
     it('renders children and handles click events', () => {
+        /*fn() method is used to create a mock function called handleClick */
         const handleClick = jest.fn();
-        const { getByText } = render(
+        // Then uses the getByText method from the React Testing Library to get
+        // reference to the button element with the text "Click me"
+        render(
             <BrowserRouter>
                 <Button onClick={handleClick}>
                     Click me
@@ -17,28 +23,41 @@ describe('Button', () => {
             </BrowserRouter>
 
         );
-        const button = getByText('Click me');
+        const button = screen.getByText('Click me');
+        // Stimulates a click event on that button
         fireEvent.click(button);
         expect(handleClick).toHaveBeenCalledTimes(1);
     });
+
+    // Test if the button element has all the expected class names from css file applied to it.
+    // Ensure that the correct styles are applied to the correct elements
 
     // Sets description for the test using it function
     it('applies the correct style and size classes', () => {
         /* render function is used to render a Button component wrapped
         in a BrowserRouter component. The button component is passed two props*/
-        const { getByRole } = render(<BrowserRouter><Button buttonStyle="btn--primary" buttonSize="btn--large">Click me</Button></BrowserRouter>);
+        render(<BrowserRouter><Button buttonStyle="btn--primary" buttonSize="btn--large">Click me</Button></BrowserRouter>);
 
-        const button = getByRole('button');
+        // getByRole() returns an element that has the role attribute with the
+        // specified value
+        const buttonElement = screen.getByRole('button');
 
         /*'toHaveClass' is a matcher that used to check whether the button
         element has the correct class names applied to it. If the button element
         has all the expected class names, the test case will pass.*/
-        expect(button).toHaveClass('btn btn--primary btn--large');
+        expect(buttonElement).toHaveClass('btn btn--primary btn--large');
     });
 
+    // This test checks whether the anchor element has the correct href attribute value
+    // applied to it.
     it('renders a link to sign up page', () => {
-        const { container } = render(<BrowserRouter><Button>Sign up</Button></BrowserRouter>);
-        const link = container.querySelector('a');
-        expect(link).toHaveAttribute('href', '/sign-up');
+        render(<BrowserRouter><Button>Sign up</Button></BrowserRouter>);
+
+        // querySelector() method returns the first element that matches
+        // a specified CSS selecor in the document
+        //const link = container.querySelector('a');
+
+        // The matcher toHaveAttribute() checks whether the anchor element has the correct atribute name and value
+        expect(screen.getByRole('link')).toHaveAttribute('href', '/sign-up');
     });
 });
--- tecttitans-app/src/components/HeroSection.test.js
+++ tecttitans-app/src/components/HeroSection.test.js
@@ -6,6 +6,13 @@ import  '@testing-library/jest-dom/extend-expect';
 
 
 describe('HeroSection component', () => {
+
+    /* This test whether a HeroSection component renders the correct title and description text content
+    *  It checks the h1 element in HeroSection contains the text
+    * "TECHTITANS VIRTUAL SUSTAINABLE TOUR
+    *  Also, it checks if the p element contains the text
+    * Experience more green accommodation from the comfort of your couch"*/
+
     it('renders the title and description correctly', () => {
         render(<BrowserRouter><HeroSection /> </BrowserRouter>);
         const titleElement = screen.getByText(/TECHTITANS VIRTUAL SUSTAINABLE TOUR/i);
@@ -14,11 +21,15 @@ describe('HeroSection component', () => {
         expect(descriptionElement).toBeInTheDocument();
     });
 
+    /* This test whether a Button component renders the correct text
+    *  It checks the button element contains the text
+    * "EXPLORE NOW!"*/
     it('renders the button with correct text', () => {
         render(<BrowserRouter><HeroSection /></BrowserRouter>);
         const buttonElement = screen.getByText(/EXPLORE NOW!/i);
         expect(buttonElement).toBeInTheDocument();
     });
+
 
     it('renders the button with correct style and size', () => {
         render(<BrowserRouter><HeroSection /></BrowserRouter>);
