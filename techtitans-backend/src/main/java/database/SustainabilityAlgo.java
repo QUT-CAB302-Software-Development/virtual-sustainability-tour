a5dbb70eae35b506d13498cf9fb9b0513402f51c
Nathan Wang <nathan123wang@gmail.com>
2023-04-21 10:54:18 +1000

    Add SustainabilityAlgo Class

--- techtitans-backend/src/main/java/database/SustainabilityAlgo.java
+++ techtitans-backend/src/main/java/database/SustainabilityAlgo.java
@@ -1,23 +1,25 @@
 package database;
 
 public class SustainabilityAlgo {
-    private int metric1;
-    private int metric2;
-    private int metric3;
-    private int metric4;
-    private int metric5;
+    private String CompanyName;
+    private int Year;
+    private int Lat;
+    private int Lng;
+    private String GICSSubIndustry;
+    private int GHGTotal;
+    private int Sales;
+    private int OperatingIncome;
 
     // Constructor for Sustainability Algorithm
-    public SustainabilityAlgo(int metric1, int metric2, int metric3, int metric4, int metric5) {
-        this.metric1 = metric1;
-        this.metric2 = metric2;
-        this.metric3 = metric3;
-        this.metric4 = metric4;
-        this.metric5 = metric5;
+    public SustainabilityAlgo(String CompanyName, int Year, int Lat, int Lng) {
+        this.CompanyName = CompanyName;
+        this.Year = Year;
+        this.Lat = Lat;
+        this.Lng = Lng;
     }
 
     public int CalculateIndex() {
-        return metric1;
+        return Year;
     }
 
 }
