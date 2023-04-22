278fb9dafb618d1cbbd5b2180fbb70148aff73d6
Nathan Wang <nathan123wang@gmail.com>
2023-04-22 19:40:17 +1000

    Company data fields

--- techtitans-backend/src/main/java/application/database/SustainabilityAlgo.java
+++ techtitans-backend/src/main/java/application/database/SustainabilityAlgo.java
@@ -1,25 +1,17 @@
 package application.database;
 
+import application.database.CompanyData;
+
+import java.util.List;
+
 public class SustainabilityAlgo {
-    private String CompanyName;
-    private int Year;
-    private int Lat;
-    private int Lng;
-    private String GICSSubIndustry;
-    private int GHGTotal;
-    private int Sales;
-    private int OperatingIncome;
+    private List<CompanyData> companyData;
 
-    // Constructor for Sustainability Algorithm
-    public SustainabilityAlgo(String CompanyName, int Year, int Lat, int Lng) {
-        this.CompanyName = CompanyName;
-        this.Year = Year;
-        this.Lat = Lat;
-        this.Lng = Lng;
+    public SustainabilityAlgo(List<CompanyData> companyData) {
+        this.companyData = companyData;
     }
 
-    public int CalculateIndex() {
-        return Year;
+    public void runCalculations() {
+        // Perform your calculations on the companyData list
     }
-
 }
