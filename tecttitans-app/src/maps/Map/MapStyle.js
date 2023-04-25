6985b5dbca55ddf5bc35f1a59a9d00482765244e
Joseph Looi <hobojLOLz@gmail.com>
2023-04-25 11:34:47 +1000

    Connect google maps API to front end (#14) - testing dependencies

--- tecttitans-app/src/maps/Map/MapStyle.js
+++ tecttitans-app/src/maps/Map/MapStyle.js
@@ -2,7 +2,7 @@ import {makeStyles} from '@mui/styles';
 
 export default makeStyles(() => ({
     paper: {
-        padding: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+        padding: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '105px',
     },
     mapContainer: {
         height: '85vh', width: '100%',
