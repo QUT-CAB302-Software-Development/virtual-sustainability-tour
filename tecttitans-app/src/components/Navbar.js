d9b2c122ee11e3ab5096ea2351534caa47bac1b2
Nathan Wang <nathan123wang@gmail.com>
2023-05-02 10:35:54 +1000

    Mobile compatability

--- tecttitans-app/src/components/Navbar.js
+++ tecttitans-app/src/components/Navbar.js
@@ -49,7 +49,7 @@ function Navbar() {
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                        <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
                             Sign up
                         </Link>
                     </li>
