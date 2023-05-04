0b773aec08fdd1b8e3f88430c41906f5ed6dcc44
Nelvin198 <75972787+Nelvin198@users.noreply.github.com>
2023-05-04 22:33:43 +1000

    Obtained at least 100 brisbane hotels using GoogleMapAPI and display them

--- tecttitans-app/src/pages/Testing.js
+++ tecttitans-app/src/pages/Testing.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "../testingMap/testingMap.css"
-import locationData from "../data/testingLocationData.json"
+import locationData from "../data/hotels_data.json"
+
+function HotelPopup({ hotel }) {
+    const apiKey = 'AIzaSyD4NREzBr7KKSr-ei0Ag85AynWOMJoS7pQ';
+    const placePhotoUrl = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${hotel.photos[0].photo_reference}&sensor=false&maxheight=400&maxwidth=250&key=${apiKey}`;
+
+
+    return (
+        <div className="hotel-popup">
+            <h3>{hotel.name}</h3>
+            <img src={placePhotoUrl} alt={hotel.name} />
+            <h3>Address: {hotel.formatted_address}</h3>
+            <h3>Rating: {hotel.rating}</h3>
+            <h3>User ratings: {hotel.user_ratings_total}</h3>
+        </div>
+    );
+}
 
 function Testing(){
 
@@ -15,12 +31,12 @@ function Testing(){
             {locationData.map(hotel => (
 
             <Marker
-                key = {hotel.id}
-                position={[hotel.gps.latitude, hotel.gps.longitude]}
+
+                position={[hotel.geometry.location.lat, hotel.geometry.location.lng]}
             >
 
                 <Popup>
-                   Hello
+                    <HotelPopup hotel={hotel} />
                 </Popup>
             </Marker>
             ))}
