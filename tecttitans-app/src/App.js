57b757a7ca40bfa36a206b2bffe4f5b52c19e8bd
Rj Otadoy <otadoyrojimjude@gmail.com>
2023-05-08 14:18:48 +1000

    Issue Ticket: https://github.com/QUT-CAB302-Software-Development/web-application-project-techtitans/issues/78
    - Installed Framer Motion
    - Added transitions between pages
    - Installed React Spinners
    - Added Loading functionality
    
    [ x ] I have performed a self-review of my code
    [ x ] I have tested the extensively tested the code
    [ x ] Added comments to code to follow best practice

--- tecttitans-app/src/App.js
+++ tecttitans-app/src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar";
 import AnimatedRoutes from "./components/AnimatedRoutes";
+import ClipLoader from "react-spinners/ClipLoader";
+
 //  established routing between the four pages of the front end
 function App() {
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+
+        }, 1000)
+    }, [])
     return (
-        <>
-            <Router>
-                <Navbar />
-                <AnimatedRoutes />
-            </Router>
-        </>
+        <div className="App">
+            {
+                loading?
+                <div className="spinner-container">
+                <ClipLoader
+                color="#36d7b7"
+                 loading={loading}
+                 size={300}/>
+                 </div>
+                      :
+                       <Router>
+                            <Navbar />
+                            <AnimatedRoutes />
+                       </Router>
+            }
+        </div>
     );
 }
 
