bdcce11ebc6f046b963e518f221d280263fcdb40
Joseph Looi <hobojLOLz@gmail.com>
2023-05-13 16:26:14 +1000

    Added Nathan's Loading Animation
    - added new Lottie loading animation (npm i lottie-react)
    - animation is placed under src/videos/LoadingAnimation.json
    - renamed Loading.js to Loader.js

--- tecttitans-app/src/App.js
+++ tecttitans-app/src/App.js
@@ -1,37 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect } from "react";
 import {BrowserRouter as Router} from "react-router-dom";
-import './App.css';
 import Navbar from "./components/Navbar";
-import Loading from "./components/Loading";
-import ClipLoader from "react-spinners/ClipLoader";
-import { CircularProgress } from '@mui/material';
+import Loading from "./components/Loader";
+import './App.css';
+
+// import ClipLoader from "react-spinners/ClipLoader";
+// import { CircularProgress } from '@mui/material';
+import Lottie from "lottie-react";
+import LoadingAnimation from './videos/LoadingAnimation.json';
 
 //  established routing between the four pages of the front end
 function App() {
 
     const [loading, setLoading] = useState(false);
-
     useEffect(() => {
         setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-
-        }, 1000)
+        setTimeout(() => {setLoading(false)}, 1000)
     }, [])
+
     return (
         <div className="App">
             {
                 loading ? 
                 <div className="spinner-container">
-                    <ClipLoader
+                    {/* <ClipLoader
                     color="#36d7b7"
                     loading={loading}
                     size={300}/>
                     <CircularProgress
                     color="primary"
                     size="16rem"
-                    thickness={1} />
-                 </div>
+                    thickness={1} /> */}
+                    <Lottie animationData={LoadingAnimation} />
+                </div>
                 :
                 <Router>
                     <Navbar />
