da46d81eaf7517ee2dd6ab81cb47e9e80cb9b644
Nelvin198 <75972787+Nelvin198@users.noreply.github.com>
2023-05-15 20:19:37 +1000

    Adding typing effect to the website's heading

--- tecttitans-app/src/pages/Home/HeroSection.js
+++ tecttitans-app/src/pages/Home/HeroSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button} from "../../components/Button";
 import hero from './HeroSection.css'
 import {Link} from 'react-router-dom';
@@ -6,7 +6,46 @@ import { motion } from 'framer-motion';
 import '../../App.css'
 import './HeroSection.css';
 
+const LoopingTypingHeading = ({ texts }) => {
+    const [textIndex, setTextIndex] = useState(0);
+    const [currentText, setCurrentText] = useState('');
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTextIndex((textIndex + 1) % texts.length);
+        }, 2000);
+
+        return () => clearInterval(intervalId);
+    }, [textIndex, texts.length]);
+
+    useEffect(() => {
+        const typingIntervalId = setInterval(() => {
+            const textToType = texts[textIndex];
+            const newText = textToType.substring(0, currentText.length + 1);
+            setCurrentText(newText);
+
+            if (newText === textToType) {
+                clearInterval(typingIntervalId);
+
+                // Start the 10-second timeout
+                setTimeout(() => {
+                    setCurrentText('');
+                }, 10000);
+            }
+        }, 100);
+
+        return () => clearInterval(typingIntervalId);
+    }, [currentText, textIndex, texts]);
+
+    return (
+        <h1 className="typing-heading">
+            {currentText}
+            <span className="typing-cursor">|</span>
+        </h1>
+    );
+};
 function HeroSection() {
+    const texts = ['TechTitans Virtual Sustainable Tour'];
     return (
         <motion.div
             className='hero-container global-padding space-small'
@@ -15,7 +54,7 @@ function HeroSection() {
             exit={{opacity: 0}}
         >
             <img src={hero} alt=""/>
-            <h1 className = 'space-small'>TechTitans Virtual Sustainable Tour</h1>
+            <LoopingTypingHeading texts={texts} />
             <p>Experience Brisbane Sustainability from your own couch</p>
             <div className="hero-btn">
                 <div className = 'space-small'>
