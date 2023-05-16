15f2142a12aef7de6fa17fb2f000fae88935dcd2
Joseph Looi <hobojLOLz@gmail.com>
2023-05-16 11:28:03 +1000

    Updated Loading animation
    -implemented Nathan's loading animation
    -pulled from master

--- tecttitans-app/src/App.js
+++ tecttitans-app/src/App.js
@@ -3,9 +3,6 @@ import {BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Loading from "./components/Loader";
 import './App.css';
-
-// import ClipLoader from "react-spinners/ClipLoader";
-// import { CircularProgress } from '@mui/material';
 import Lottie from "lottie-react";
 import LoadingAnimation from './videos/LoadingAnimation.json';
 
@@ -23,14 +20,6 @@ function App() {
             {
                 loading ? 
                 <div className="spinner-container">
-                    {/* <ClipLoader
-                    color="#36d7b7"
-                    loading={loading}
-                    size={300}/>
-                    <CircularProgress
-                    color="primary"
-                    size="16rem"
-                    thickness={1} /> */}
                     <Lottie animationData={LoadingAnimation} />
                 </div>
                 :
