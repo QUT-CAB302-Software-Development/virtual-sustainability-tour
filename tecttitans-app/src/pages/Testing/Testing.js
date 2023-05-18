ff50d6688598ba0b65b45e8c95e70ee720110910
Nathan Wang <nathan123wang@gmail.com>
2023-05-18 15:09:00 +1000

    Lock 3d View

--- tecttitans-app/src/pages/Testing/Testing.js
+++ tecttitans-app/src/pages/Testing/Testing.js
@@ -8,12 +8,6 @@ import { motion } from 'framer-motion';
 import "./Testing.css";
 
 function Testing() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.76, lng: -73.983 },
-        zoom: 15,
-        mapTypeId: "satellite",
-    });
 
-    map.setTilt(45);
 }
 export default Testing;
