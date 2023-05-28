aa9d9c061692ef590eb7e5431bc715eb63a5e552
Nathan Wang <nathan123wang@gmail.com>
2023-05-28 10:06:09 +1000

    Update ExplainsESG + markdown

--- tecttitans-app/src/pages/Tour/ExplainESG.js
+++ tecttitans-app/src/pages/Tour/ExplainESG.js
@@ -10,9 +10,6 @@ import './ExplainESG.css';
 
 const desc = [
     "ESG scores assess a company's performance and impact in areas related to the environment, society, and governance (ESG).",
-    "First, the average ratios for key environmental metrics such as green house gas emissions, water withdrawal, water discharge and operating income across companies in the industry are determined. These ratios serve as benchmarks for comparison.",
-    "Then, a normalized score for each metric based on its impact on the company's performance and the industry's average performance is calculated. In addition to the previously described metrics, emissions like SOx, NOx, and VOC are also taken into account.",
-    "Weights are assigned to each metric to reflect its importance. The normalized scores for each metric are multiplied by their respective weights, and the weighted scores are summed up to obtain an overall ESG score for the company. This score is rounded, multiplied by a normalization factor, and returned as the final result."
 ]
 
 
